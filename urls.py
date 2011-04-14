88ce59015c13fa44a9ff10c4be76f9befed92ec5
lfborjas <luis.borjas@escolarea.com>
2011-04-14 00:23:27 -0600

    creadas urls

--- urls.py
+++ urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'intro_django.views.home', name='home'),
-    # url(r'^intro_django/', include('intro_django.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^/snippets$', 'pastebin.views.manage', name='snippet_list'),
+    url(r'^/snippets/new$', 'pastebin.views.create', name='snippet_create'),
+    url(r'^/snippets/(?P<snippet_id>\d+)$', 'pastebin.views.show', name='snippet_show'),
 )
