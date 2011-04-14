77f2b76e0ceff2221210c5b3911145a819fcc891
lfborjas <luis.borjas@escolarea.com>
2011-04-13 23:47:49 -0600

    creado modelo de pastebin

--- pastebin/models.py
+++ pastebin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Paste(models.Model):
+    LANGS = (
+                ('py', 'Python'),
+                ('lisp', 'Lisp'),
+                ('js', 'Javascript'),
+                ('rb', 'Ruby'),
+                ('css', 'CSS'),
+                ('html', 'HTML')
+            )
+
+    title = models.CharField(max_length=128, default = "untitled")
+    lang  = models.CharField(max_length=10, choices = LANGS)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
